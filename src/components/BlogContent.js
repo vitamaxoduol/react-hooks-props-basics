32a2f09d40c4e2f2062c4ed8e6c53ce26be6b090
vitamaxoduol <vitamaxoduo@gmail.com>
2023-07-19 11:58:03 +0300

    Completed lab on Props Basic

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props); 
+  /*{ articleText: "Dear Reader: Bjarne Stroustrup has the perfect lecture oration." 
+  isPublished=true
+  minutesToRead= 1 
 }
+*/
 
+
+if (!props.isPublished) {
+  //hide the published content
+  //return null means "don't  display any DOM element here"
+  return null;
+}
+else {
+  //Show pulished content
+
+  return (
+   <div id="blog-content">
+      <h1>{props.articleText}</h1>
+      <p>{props.minutesToRead} minutes to read</p>
+   </div>
+  );
+}
+}
 export default BlogContent;
